39d7fdb7f0fb76223a718425da57663df49f01c4
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-08-11 17:18:59 -0300

    config initials and add theme

---
+++ android/app/src/main/kotlin/com/techmathues/app_endcep_ft/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.techmathues.app_endcep_ft
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EndCepApp());
+}
+
+class EndCepApp extends StatelessWidget {
+  const EndCepApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Consula de CEP',
+      home: Container(),
+    );
+  }
+}
